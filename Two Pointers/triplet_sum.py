0673ef677bcc331fd8458176e013b6c79593dab1
Rohan <pudasainirohan0@gmail.com>
2026-06-25 15:43:38 +0545

    refactor: rename triplet_sum to triplet_sum_bruteforce, update test harness to accept callables, and add new test case

--- Two Pointers/triplet_sum.py
+++ Two Pointers/triplet_sum.py
@@ -1,4 +1,7 @@
-def triplet_sum(array: list[int]) -> list[list[int]]:
+from collections.abc import Callable
+
+
+def triplet_sum_bruteforce(array: list[int]) -> list[list[int]]:
     length = len(array)
     triplets = set()
     for i in range(length):
@@ -9,9 +12,9 @@ def triplet_sum(array: list[int]) -> list[list[int]]:
     return [list(triplet) for triplet in triplets]
 
 
-def test(cases: list[list[int]]):
+def test(function: Callable[[list[int]], list[list[int]]], cases: list[list[int]]):
     for case in cases:
-        results = triplet_sum(case)
+        results = function(case)
         print(f"{case=}")
         print(f"{results=}")
         print([sum(result) for result in results])
@@ -27,6 +30,7 @@ cases = [
     [0, 0, 0],
     [1, 0, 1],
     [0, 0, 1, -1, -1, -1],
+    [0, 0, 0, 2, 0, -2, 3, -2, -1],
 ]
 
-test(cases)
+test(function=triplet_sum_bruteforce, cases=cases)
