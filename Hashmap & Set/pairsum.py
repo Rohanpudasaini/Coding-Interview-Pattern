e598c467e6164580b8f48621cdb0024af5ee7439
Rohan <pudasainirohan0@gmail.com>
2026-07-05 23:45:55 +0545

    feat: also add option for multiple pairs in the array

--- Hashmap & Set/pairsum.py
+++ Hashmap & Set/pairsum.py
@@ -1,8 +1,8 @@
-def pairsum_bruteforce(array: list[int], sum: int) -> list[int]:
+def pairsum_bruteforce(array: list[int], target: int) -> list[int]:
     length = len(array)
     for i in range(length):
         for j in range(i + 1, length):
-            if array[i] + array[j] == sum:
+            if array[i] + array[j] == target:
                 return [i, j]
     return []
 
@@ -10,18 +10,34 @@ def pairsum_bruteforce(array: list[int], sum: int) -> list[int]:
 # Time complexity is O(n^2), space complexity is O(1)
 
 
-def pairsum(array: list[int], sum: int) -> list[int]:
+def pairsum(array: list[int], target: int) -> list[int]:
     hash_set = {}
     for i in range(len(array)):
         if hash_set.get(array[i]) is not None:
             return [hash_set[array[i]], i]
-        hash_set[sum - array[i]] = i
+        hash_set[target - array[i]] = i
     return []
 
 
 # Time complexity is O(N) and space complexity is also O(N)
 
-test_case = [-1, 3, 4, 2]
-sum = 3
+test_case = [-1, 3, 4, 2, 0, 1]
+target = 3
 # print(pairsum_bruteforce(test_case, sum))
-print(pairsum(test_case, sum))
+print(pairsum(test_case, target))
+
+
+def multiple_pairsum(array: list[int], target: int) -> list[list[int]]:
+    hash_set = {}
+    result = set()
+    for i in range(len(array)):
+        if hash_set.get(array[i]) is not None:
+            pair = [hash_set[array[i]], i]
+            pair.sort()
+            pair = tuple(pair)
+            result.add(pair)
+        hash_set[target - array[i]] = i
+    return [list(x) for x in result]
+
+
+print(multiple_pairsum(test_case, target))
