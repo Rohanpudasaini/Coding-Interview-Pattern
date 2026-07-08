3b33e18b4b57585b00b2cc02df281cc0a9557c80
Rohan <pudasainirohan0@gmail.com>
2026-07-09 00:26:19 +0545

    feat: add O(1) space complexity implementation for zero-striping matrix algorithm

--- Hashmap & Set/zero_striping.py
+++ Hashmap & Set/zero_striping.py
@@ -1,4 +1,5 @@
 import random
+from copy import deepcopy
 
 def random_matrix(m,n, upper_bound=6, lower_bound=0):
     return [[random.randint(lower_bound, upper_bound) for _ in range(n)] for _ in range(m)]
@@ -7,6 +8,7 @@ def random_matrix(m,n, upper_bound=6, lower_bound=0):
 
 
 matrix = random_matrix(5,5)
+new_matrix = deepcopy(matrix)
 for r in matrix:
     print(r)
 
@@ -32,6 +34,45 @@ def zero_striping(mat:list[list[int]])-> list[list[int]]:
 # Time complexity is O(M*N) where M and N is size of the matrix
 # Space complexity is O(M+N)
 
+# Optimal solution will need to have lower space complexity.
+
+def zero_striping_optimal(mat: list[list[int]]) -> list[list[int]]:
+    row = len(mat)
+    col = len(mat[0])
+
+    first_row_contain_zero = False
+    for i in range(row):
+        if mat[i][0] == 0:
+            first_row_contain_zero = True
+            break
+    
+    first_column_contain_zero = False
+    for j in range(col):
+        if mat[0][j] == 0:
+            first_column_contain_zero = True
+            break
+
+    for i in range(1, row):
+        for j in range(1, col):
+            if mat[i][j] == 0:
+                mat[i][0] = 0
+                mat[0][j] = 0
+    
+    for i in range(1, row):
+        for j in range(1, col):
+            if mat[i][0] == 0 or mat[0][j] == 0:
+                mat[i][j] = 0
+    
+    if first_column_contain_zero:
+        for i in range(col):
+            mat[0][i] = 0
+    
+    if first_row_contain_zero:
+        for i in range(row):
+            mat[i][0] = 0
+
+    return mat
+
 
 
 zero_striping(matrix)
@@ -39,4 +80,14 @@ zero_striping(matrix)
 print("*****"*20)
 
 for r in matrix:
+    print(r)
+print("*****"*20)
+for r in new_matrix:
+    print(r)
+
+zero_striping_optimal(new_matrix)
+
+print("*****"*20)
+
+for r in new_matrix:
     print(r)
