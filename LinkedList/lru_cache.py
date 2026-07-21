e731de45e0ee0de482e24323539ba155cda35141
Rohan <pudasainirohan0@gmail.com>
2026-07-21 11:00:03 +0545

    feat: implement LRU cache eviction logic and core node management methods

--- LinkedList/lru_cache.py
+++ LinkedList/lru_cache.py
@@ -1,28 +1,53 @@
 class ListNode:
-    def __init__(self, value, previous, next):
+    def __init__(self, key, value):
+        self.key = key
         self.value = value
-        self.previous = previous
-        self.next = next
+        self.previous = None
+        self.next = None
 
 class LRUCache:
     def __init__(self, capacity:int):
         self.capacity = capacity
-        self.head = ListNode(0,None,None)
-        self.tail = ListNode(0,None,None)
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.previous = self.head
         self.cache = {}
 
     
     def get(self,key:int):
-        pass
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove_node(node)
+        self.add_node(node)
+        return node.value
     
     def put(self, key:int, value:int):
-        pass
-    
+        if key in self.cache:
+            self.remove_node(self.cache[key])
+            del self.cache[key]
+
+        if len(self.cache) >= self.capacity:
+            last_node = self.head.next
+            self.remove_node(last_node)
+            del self.cache[last_node.key]
+        node_to_add = ListNode(key, value)
+        self.add_node(node_to_add)
+        self.cache[key] = node_to_add
+
     def remove_node(self, node:ListNode):
-        pass
+        prev_node = node.previous
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.previous = prev_node
     
     def add_node(self, node:ListNode):
-        pass
+        previous_node = self.tail.previous
+        previous_node.next = node
+        node.previous = previous_node
+        node.next = self.tail
+        self.tail.previous = node
     
 
     
